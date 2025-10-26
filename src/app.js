216f75cb2fdeb12669c0543344465db40e91012b
unknown <samuka1999cristovao@gmail.com>
2025-10-26 19:27:33 -0300

    update001

--- src/app.js
+++ src/app.js
@@ -5,16 +5,28 @@ import morgan from "morgan";
 import actionsRoutes from "./routes/actions.routes.js";
 
 const app = express();
+
 app.use(cors({
-    origin: ["https://acoessociais.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+  origin: [
+    "https://cadastrodeacoessociais.vercel.app",
+    "http://localhost:5173" 
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 }));
+
+
 app.use(helmet());
 app.use(morgan("dev"));
+
+
 app.use(express.json());
 
+
 app.get("/health", (_req, res) => res.json({ status: "ok" }));
+
+
 app.use("/api/actions", actionsRoutes);
 
 export default app;
